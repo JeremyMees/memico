1ae18e40d9d3383c8dbf470b9cea00ae6b212b19
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-05-24 20:37:18 +0200

    changed seo

--- app.vue
+++ app.vue
@@ -11,3 +11,71 @@
     <Footer />
   </div>
 </template>
+
+<script setup lang="ts">
+import config from '@/constants/seo'
+
+const {
+  url,
+  name,
+  logo,
+  description,
+} = config.meta
+
+const head = computed(() => ({
+  titleTemplate: () => name,
+  name,
+  link: [
+    {
+      rel: 'canonical',
+      href: url,
+    },
+  ],
+  meta: [
+    {
+      id: 'description',
+      name: 'description',
+      content: description,
+    },
+    {
+      id: 'robots',
+      name: 'robots',
+      content: 'index,follow',
+    },
+    {
+      id: 'twitter:title',
+      name: 'twitter:title',
+      content: name,
+    },
+    {
+      id: 'twitter:description',
+      name: 'twitter:description',
+      content: description,
+    },
+    {
+      id: 'twitter:image',
+      name: 'twitter:image',
+      content: logo,
+    },
+    {
+      id: 'og:image',
+      property: 'og:image',
+      content: logo,
+    },
+    {
+      id: 'og:title',
+      property: 'og:title',
+      content: name,
+    },
+    {
+      id: 'og:description',
+      property: 'og:description',
+      content: description,
+    },
+  ],
+}))
+
+useHead(head.value)
+
+useSchemaOrg([defineWebPage({ name, url })])
+</script>
