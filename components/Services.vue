7db5f3dd64c91b924b92a60d6cf8ccda2edb9371
Jeremy Mees <59842057+JeremyMees@users.noreply.github.com>
2024-06-15 09:57:03 +0200

    content update

--- components/Services.vue
+++ components/Services.vue
@@ -28,18 +28,6 @@
           Grondwerken omvatten het voorbereiden, uitgraven, aanvullen, draineren, verdichten, nivelleren en verharden van de bodem voor bouwprojecten. Dit proces zorgt voor een stabiele basis en wordt afgesloten met een controle om te voldoen aan de specificaties.
         </p>
       </div>
-      <div
-        class="shadow border border-slate-900 rounded-lg p-6 space-y-2 relative bottom-0 hover:bottom-1 transition-all duration-300 ease-in-out"
-        data-aos="fade-up"
-        data-aos-delay="150"
-      >
-        <h3>
-          Algemene bouwwerken
-        </h3>
-        <p>
-          Algemene bouwwerken omvatten het ontwerpen, plannen en construeren van gebouwen en infrastructuur. Dit proces begint met het opstellen van bouwtekeningen en vergunningen, gevolgd door funderingswerkzaamheden, opbouw van de structuur, installatie van nutsvoorzieningen en afwerking. Het doel is om functionele, veilige en duurzame constructies te realiseren die voldoen aan de eisen en wensen van de opdrachtgever.
-        </p>
-      </div>
     </div>
   </div>
 </template>
